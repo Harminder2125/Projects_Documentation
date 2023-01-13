94757d02cc1cb6c95e8812c17b6ee436027c5fcd
Harminder Singh <harminder.singh2125@gmail.com>
2023-01-13 13:14:39 +0530

    msg

--- app/Http/Livewire/GroupComponent.php
+++ app/Http/Livewire/GroupComponent.php
@@ -9,19 +9,14 @@ use App\Models\Group;
 class GroupComponent extends Component
 {
     public $searchGroup="";
-    public $refresh = false;
-
+    public $editmode = false;
+    public $confirmingGroupDeletion = false;
     public $group=[
+    "id"=>"",
     "name"=>"",
     "code"=>""
    ];
-   public function toggle($key)
-   {
-
-    // if($key =="refresh")
-        $this->refresh = !$this->refresh;
-
-   }
+   
     public function render()
     {
         $groups = Group::when($this->searchGroup,function($query, $searchGroup){
@@ -32,6 +27,12 @@ class GroupComponent extends Component
             'allgroups'=>$groups,
         ]);
     }
+    public function toggle($key)
+    {
+        if($key == "Deletion")
+            $this->confirmingGroupDeletion = !$this->confirmingGroupDeletion;
+    }
+
     public function addGroup()
     {
         Validator::make($this->group, [
@@ -46,6 +47,68 @@ class GroupComponent extends Component
         ]);
       
         $this->emit('close-banner');
-        $this->toggle('refresh');
+        $this->group=[];
     }
+     public function openGroupForEditing($id)
+    {
+        
+        $editgroup = Group::find($id);
+        $this->group['id'] = $editgroup->id;
+        $this->group['name'] = $editgroup->name;
+        $this->group['code'] = $editgroup->code;
+        $this->editmode = true;
+ 
+    }
+    public function cancelEditMode()
+    {
+        $this->editmode = false;
+        $this->group=[];
+    }
+    public function updateGroup($id)
+    {
+        Validator::make($this->group, [
+            'name' => ['required', 'string', 'max:255'],
+            'code'=>['required','string','unique:groups,code,'.$id]
+        ])->validate();
+        
+        $group_update = Group::find($id);
+        $group_update->name = $this->group['name'];
+        $group_update->code = $this->group['code'];
+       
+        $group_update->save();
+      
+    
+        $this->dispatchBrowserEvent('banner-message', [
+            'style' => 'success',
+            'message' => 'Group details successfully updated!'
+        ]);
+      
+        $this->emit('close-banner');
+        $this->editmode = false;
+    }
+     public function openGroupForDeletion($id)
+    {
+        
+        $editgroup = Group::find($id);
+        $this->group['id'] = $id;
+        $this->group['name'] = $editgroup->name;
+        $this->group['code'] = $editgroup->code;
+        $this->toggle('Deletion');
+
+    }
+     public function deleteGroup($id)
+    {
+        
+        $group_delete = Group::find($id);
+        $group_delete->delete();
+        
+        $this->toggle('Deletion');
+        $this->dispatchBrowserEvent('banner-message', [
+            'style' => 'danger',
+            'message' => 'Group successfully deleted!'
+        ]);
+      
+        $this->emit('close-banner');
+    }
+
 }
