7e35f9618faa23c4237c259bc8ed3cdcdc490e20
Harminder Singh <harminder.singh2125@gmail.com>
2023-01-26 22:13:41 +0530

    kk

--- resources/views/livewire/unpublished-projects-component.blade.php
+++ resources/views/livewire/unpublished-projects-component.blade.php
@@ -2,9 +2,9 @@
    <p>These projects have been Unpublished by you (Admin) and are not visible on the portal. </p>
    <div class="grid md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-1 grid-flow-row gap-4 mt-5">
     @foreach($projects as $project)
-    <div class="p-4 border border-pink-300 bg-pink-100 rounded"><h1>{{$project->title}}</h1>
+    <div class="p-4 border border-yellow-300 bg-yellow-100 rounded"><h1>{{$project->title}}</h1>
     <div class="flex justify-end text-sm">
-        <a href="#" class="text-pink-500 hover:text-pink-800">open..</a>
+        <a href="#" class="text-yellow-500 hover:text-yellow-800">open..</a>
     </div>
     </div>
     @endforeach
