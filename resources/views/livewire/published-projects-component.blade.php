0589693e4504445096bd75db5eb94cb26e3b09e3
Harminder Singh <harminder.singh2125@gmail.com>
2023-02-01 13:43:32 +0530

    fgf

--- resources/views/livewire/published-projects-component.blade.php
+++ resources/views/livewire/published-projects-component.blade.php
@@ -1,12 +1,65 @@
 <div>
-   <p>These projects have been approved by you (Admin) and are visible on the portal. </p>
-   <div class="grid md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-1 grid-flow-row gap-4 mt-5">
-    @foreach($projects as $project)
-    <div class="p-4 border border-emerald-300 bg-emerald-100 rounded"><h1>{{$project->title}}</h1>
-    <div class="flex justify-end text-sm">
-        <a href="#" class="text-emerald-500 hover:text-emerald-800">open..</a>
-    </div>
-    </div>
-    @endforeach
+    <p>These projects have been approved by you (Admin) and are visible on the portal. </p>
+    <div class="grid md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-1 grid-flow-row gap-4 mt-5">
+        @foreach($projects as $project)
+        <div class="border-0 border-emerald-300 bg-gray-100 rounded-lg flex flex-col flex-center shadow-lg">
+            <div class="overflow-hidden h-32 rounded-t-lg relative">
+                <span class="absolute top-0 right-0 px-2 pb-0.5 text-xs  font-semibold text-gray-600 mt-4 mr-4 bg-purple-200 rounded-lg">Category</span>
+                <img src="./assets/images/projects/banner3.jpg" class="w-full h-32 object-cover" />
+            </div>
+            <div class="-mt-10 flex justify-center overflow-hidden z-10">
+
+                <img src="./assets/images/projects/dummylogo.png" class="border-4 border-white flex justify-center items-center w-20 h-20 rounded-full shadow-sm object-cover" />
+
+
+
+            </div>
+            <div class="py-1 ">
+                <h1 class="font-semibold uppercase text-sm text-gray-800 text-center h-8">{{$project->title}}</h1>
+                <div class="flex justify-center mt-3 flex-col items-center text-xs text-gray-500">
+
+                    <h1 class="font-semibold uppercase text-xs text-gray-700 text-center">
+                        Handled by
+                    </h1>
+                    <span class="mb-3"> Division -1 (NIC Punjab)</span>
+                    <h1 class="font-semibold uppercase text-xs text-gray-700 text-center">
+                        HOD Name
+                    </h1>
+                    <span class="mb-3">Mukesh Kumar Ralli</span>
+
+                    <h1 class="font-semibold uppercase text-xs text-gray-700 text-center">
+                        Completion Status
+                    </h1>
+
+                    <div class="w-full px-8">
+                        <div class="w-full h-4 pt-1">
+                            <div class="overflow-hidden h-4 mb-4 text-xs flex rounded bg-rose-200">
+                                <div style="width:80%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gradient-to-r from-fuchsia-900 via-pink-800 to-red-700">80%</div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+
+
+
+
+                <div class="mt-4">
+                    <div class="flex justify-center text-sm bg-gray-200 w-full p-3 ">
+                        <a href="#" class="text-gray-500 hover:text-gray-800">More details</a>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+        @endforeach
     </div>
 </div>
