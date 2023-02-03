15065dfdd1633508789b34c8abc585ba03912a99
kASH6211 <119485909+kASH6211@users.noreply.github.com>
2023-02-03 13:27:59 +0530

    featurebox

---
+++ database/migrations/2023_02_03_060612_create_feature_box_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('featurebox', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained('projects')->onDelete('restrict');
+            $table->string('title',150);
+            $table->string('subtitle',300);
+            $table->integer('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('featurebox');
+    }
+};
