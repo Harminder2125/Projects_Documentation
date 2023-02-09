6c01bf364850fa9f5020a3c33e7961a26c37bfaf
Harminder Singh <harminder.singh2125@gmail.com>
2023-02-09 17:38:54 +0530

    bnv

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Models\Project;
 
 class UserController extends Controller
 {
@@ -12,15 +14,15 @@ class UserController extends Controller
     }
     public function manageprojects()
     {
-        /* If user is HOD of any division we will retrieve all the projects of that division */
-       $divisions = Auth::user()->divisions->pluck('id')->toArray();
-       $projects = null;
+    //     /* If user is HOD of any division we will retrieve all the projects of that division */
+    //    $divisions = Auth::user()->divisions->pluck('id')->toArray();
+    //    $projects = null;
        
-       if(count($divisions)>0)
-        {    
-             $projects = Project::whereIn('division_id',$divisions)->get();
+    //    if(count($divisions)>0)
+    //     {    
+    //          $publishedprojects = Project::Published()->whereIn('division_id',$divisions)->get();
             
-        }
+    //     }
         
         return view('user.manageprojects',['projects'=>$projects]);
     }
