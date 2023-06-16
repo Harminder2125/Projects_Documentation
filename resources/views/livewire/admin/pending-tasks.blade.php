2deccc5b50d6436ee90653044fe7615bd650e743
Harminder Singh <harminder.singh2125@gmail.com>
2023-06-16 09:06:50 +0530

    gg

--- resources/views/livewire/admin/pending-tasks.blade.php
+++ resources/views/livewire/admin/pending-tasks.blade.php
@@ -1,4 +1,30 @@
 <div>
+    @if(Auth::user()->isAdmin())
+    <div class="bg-stone-200 p-4 flex justify-end items-center w-full mb-2">
+        <div class="flex items-center mr-4">
+            <div class="w-4 h-4 bg-red-700/90 mr-1 mt-0 rounded-md"></div>
+            <x-sub-title class="!text-xs font-semibold !text-red-700">Pending at your end</x-sub-title>
+        </div>
+        <div class="flex items-center">
+            <div class="w-4 h-4 bg-orange-500/90 mr-1 mt-0 rounded-md"></div>
+            <x-sub-title class="!text-xs font-semibold !text-orange-500">Pending at Project Head end</x-sub-title>
+        </div>
+
+    </div>
+    @else
+    <div class="bg-stone-200 p-4 flex justify-end items-center w-full mb-2">
+        <div class="flex items-center mr-4">
+            <div class="w-4 h-4 bg-red-700/90 mr-1 mt-0 rounded-md"></div>
+            <x-sub-title class="!text-xs font-semibold !text-red-700">Pending at your end</x-sub-title>
+        </div>
+        <div class="flex items-center">
+            <div class="w-4 h-4 bg-green-700/90 mr-1 mt-0 rounded-md"></div>
+            <x-sub-title class="!text-xs font-semibold !text-green-600">Submitted to Admin</x-sub-title>
+        </div>
+
+    </div>
+
+    @endif
     <div class="grid grid-cols-5 gap-x-4 gap-y-10">
         @foreach($projects as $prj)
         <div class=" bg-white rounded-md shadow-lg h-104">
@@ -294,7 +320,8 @@
                 @endif
                 @else
                 @if($prj->publish_status == 1)
-                <a href="/admin/edit/project/{{$prj->id}}" ><x-secondary-button class="!px-2 !py-2 !text-xs">Update & Submit Detail</x-secondary-button>
+                <a href="/admin/edit/project/{{$prj->id}}">
+                    <x-secondary-button class="!px-2 !py-2 !text-xs">Update & Submit Detail</x-secondary-button>
                 </a>
                 @elseif($prj->publish_status ==2)
                 <div class="flex flex-col justify-center items-center">
